42d2377179f49ba55fc6524e3c0e2a4a1c500c3e
Ubuntu <ubuntu@ip-172-31-28-225.ap-south-1.compute.internal>
2021-04-21 04:33:26 +0000

    initial commit to branch

--- src/utils/ErrorMsg.js
+++
@@ -1,9 +0,0 @@
-let getErrorMessage = function (field) {
-    var response = {
-        success: false,
-        message: field + ' field is missing or Invalid in the request'
-    };
-    return response;
-}
-
-module.exports = getErrorMessage
--- src/utils/splitOrgName.js
+++
@@ -1,4 +0,0 @@
-//it converts the name 'username.org.csr.com' to 'username'
-const splitOrgName = orgFullName => orgFullName.split('.')[0];
-
-module.exports = splitOrgName
