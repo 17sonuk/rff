aa7dba6e56f221644244628f3b3a72ca3b6234c9
Ubuntu <ubuntu@ip-172-31-1-154.ap-south-1.compute.internal>
2021-10-25 08:53:47 +0000

    ssl fix

--- CSR_Express/index.js
+++ CSR_Express/index.js
@@ -38,12 +38,12 @@ app.get('/test', (req, res, next) => {
     res.send('success!!!!!!!!!!')
 })
 
-const credentials = { key: privateKey, cert: certificate }; // https
+// const credentials = { key: privateKey, cert: certificate }; // https
 
-const httpsServer = https.createServer(credentials, app); // https
+// const httpsServer = https.createServer(credentials, app); // https
 
-httpsServer.listen(PORT, () => logger.info(`Server running on Port ${PORT} (HTTPS)`)); //https
+// httpsServer.listen(PORT, () => logger.info(`Server running on Port ${PORT} (HTTPS)`)); //https
 
-// app.listen(PORT, () => {
-//     logger.info(`Application running on port ${PORT} (HTTP)`);
-// })
+app.listen(PORT, () => {
+    logger.info(`Application running on port ${PORT} (HTTP)`);
+})
